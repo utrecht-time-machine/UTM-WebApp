ca910711cf46e94f61becffaffbb2e1b160019cd
UTM @ vps <r.companje@hetutrechtsarchief.nl>
2022-07-04 13:14:29 +0200

    changes until 4 july

---
+++ sites/all/themes/utm/templates/paragraphs/paragraphs-item--series.tpl.php
@@ -0,0 +1,24 @@
+<?php
+$head = (isset($content['field_head']) ? $content['field_head']['#items'][0]['value'] : '');
+$lang = (isset($content['field_lang']) ? $content['field_lang']['#items'][0]['value'] : 'nl');
+$view = views_get_view('misc');
+$view_disp = 'series';
+$view->set_display($view_disp);
+$view->set_arguments(array($lang));
+$view = $view->preview();
+?>
+<div class="block-series">
+	<div class="band">
+<?php if($head): ?>
+		<div class="section-header">
+			<div>
+				<h2><?= $head; ?></h2>
+			</div>
+			<div></div>
+		</div>
+<?php endif; ?>
+		<div class="view-card">
+			<?= $view; ?>
+		</div>
+	</div>
+</div>
